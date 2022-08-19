ddfde26718fdbe2ede1eed6701337df5cd09eba7
myungjin.kim <darkkmj@gmail.com>
2022-08-19 23:37:47 +0900

    Update a factory pattern sample code

--- FactoryPattern/FactoryPattern.cpp
+++ FactoryPattern/FactoryPattern.cpp
@@ -10,7 +10,7 @@ public:
 	Pizza() = default;
 	~Pizza() = default;
 
-	void prepare() {
+	virtual void prepare() {
 		std::cout << "Preparing " << name << std::endl;
 		std::cout << "Tossing dough... " << std::endl;
 		std::cout << "Adding sauce..." << std::endl;
@@ -20,15 +20,15 @@ public:
 		}
 	}
 
-	void bake() {
+	virtual void bake() {
 		std::cout << "Bake for 25 minutes at 350" << std::endl;
 	}
 
-	void cut() {
+	virtual void cut() {
 		std::cout << "Cutting the pizza into diagonal slices" << std::endl;
 	}
 
-	void box() {
+	virtual void box() {
 		std::cout << "Place pizza in official PizzaStore box" << std::endl;
 	}
 
@@ -36,16 +36,96 @@ public:
 		return name;
 	}
 
-private:
+protected:
 	std::string name;
 	std::string dough;
 	std::string sauce;
 	std::list<std::string> toppings;
 };
 
+class NYStyleCheesePizza : public Pizza {
+public:
+	NYStyleCheesePizza() {
+		name = "NY Style Sauce and Cheese Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+
+		toppings.push_back("Grated Reggiano Cheese");
+	}
+};
+
+class ChicagoStyleCheesePizza : public Pizza {
+public:
+	ChicagoStyleCheesePizza() {
+		name = "Chicago Style Deep Dish Cheese Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Plum Tomato Sauce";
+
+		toppings.push_back("Shredded Mozzarella Cheese");
+	}
+
+	void cut() override {
+		std::cout << "Cutting the pizza into square slices" << std::endl;
+	}
+};
+
+class PizzaStore abstract {
+public:
+	Pizza* orderPizza(std::string type) {
+		Pizza* pizza;
+		pizza = createPizza(type);
+		pizza->prepare();
+		pizza->bake();
+		pizza->cut();
+		pizza->box();
+
+		return pizza;
+	}
+
+protected:
+	virtual Pizza* createPizza(std::string type) abstract;
+};
+
+class NYPizzaStore : public PizzaStore {
+	Pizza* createPizza(std::string type) override {
+		Pizza* pizza;
+
+		if (type == "cheese") {
+			pizza = new NYStyleCheesePizza();
+		}
+		else {
+			std::cerr << "Unknown pizza !!!";
+		}
+
+		return pizza;
+	}
+};
+
+class ChicagoPizzaStore : public PizzaStore {
+	Pizza* createPizza(std::string type) override {
+		Pizza* pizza;
+
+		if (type == "cheese") {
+			pizza = new ChicagoStyleCheesePizza();
+		}
+		else {
+			std::cerr << "Unknown pizza !!!";
+		}
+
+		return pizza;
+	}
+};
+
 int main()
 {
+	PizzaStore* nyStore = new NYPizzaStore();
+	PizzaStore* chicagoStore = new ChicagoPizzaStore();
 
+	Pizza* pizza = nyStore->orderPizza("cheese");
+	std::cout << "Ethan ordered a " << pizza->getName() << std::endl;
+
+	pizza = chicagoStore->orderPizza("cheese");
+	std::cout << "Joel ordered a " << pizza->getName() << std::endl;
 
 	return 0;
 }
